88844fd7b62bfdb8eaea3d65e2781f149180a676
Daniel Ekström <de222in@student.lnu.se>
2022-12-05 09:50:30 +0100

    day5

--- day5.py
+++ day5.py
@@ -1,46 +1,39 @@
 from textwrap import wrap
 import re
 
-def Part1():
+def Do_task(task):
   lst = []
   created = False
   with open('txtinput\day5.txt', 'r') as f:
     for line in f.readlines():
-      modifiied = wrap(line.replace("\n", " ").replace(" ", ","), 4)
-      lst, created = createLst(lst, len(modifiied), created)
-      for i in range(len(modifiied)):
-        if(modifiied[i].startswith("[")):
-          lst[i].insert(0, modifiied[i].replace(",", "").replace("[", "").replace("]", ""))
-        elif(modifiied[i].startswith("move")):
+      modified = wrap(line.replace("\n", " ").replace(" ", ","), 4)
+      lst, created = createLst(lst, len(modified), created)
+      lst = task(line, modified, lst)
+            
+  return ToString(lst)
+
+def Part1(line, modified, lst):
+  for i in range(len(modified)):
+        if(modified[i].startswith("[")):
+          lst[i].insert(0, modified[i].replace(",", "").replace("[", "").replace("]", ""))
+        elif(modified[i].startswith("move")):
           line = [int(s) - 1 for s in re.findall(r'\d+', line)]
           for i in range(line[0] + 1):
             lst[line[2]].append(lst[line[1]].pop())
-  s = ""
-  for i in lst:
-    s += i.pop()
-  return s
+  return lst
 
 
-def Part2():
-  created = False
-  lst = []
-  with open('txtinput\day5.txt', 'r') as f:
-    for line in f.readlines():
-      modifiied = wrap(line.replace("\n", " ").replace(" ", ","), 4)
-      lst, created = createLst(lst, len(modifiied), created)
-      for i in range(len(modifiied)):
-        if(modifiied[i].startswith("[")):
-          lst[i].insert(0, modifiied[i].replace(",", "").replace("[", "").replace("]", ""))
-        elif(modifiied[i].startswith("move")):
-          line = [int(s) - 1 for s in re.findall(r'\d+', line)]
-          templst = []
-          for i in range(line[0] + 1):
-            templst.insert(0, lst[line[1]].pop())
-          lst[line[2]].extend(templst)
-  s = ""
-  for i in lst:
-    s += i.pop()
-  return s
+def Part2(line, modified, lst):
+  for i in range(len(modified)):
+    if(modified[i].startswith("[")):
+      lst[i].insert(0, modified[i].replace(",", "").replace("[", "").replace("]", ""))
+    elif(modified[i].startswith("move")):
+      line = [int(s) - 1 for s in re.findall(r'\d+', line)]
+      templst = []
+      for i in range(line[0] + 1):
+        templst.insert(0, lst[line[1]].pop())
+      lst[line[2]].extend(templst)
+  return lst
 
 
 def createLst(lst, size, created):
@@ -51,6 +44,11 @@ def createLst(lst, size, created):
     created = True
   return lst, created
 
+def ToString(lst):
+  s = ""
+  for i in lst:
+    s += i.pop()
+  return s
   
-print(Part1())
-print(Part2())
+print(Do_task(Part1))
+print(Do_task(Part2))
